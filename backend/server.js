cac208e368c268bf7c52d9428ed9ead7a723e704
Eder-Rafael-Avila <ra52834094810@acaonsfatima.org.br>
2026-09-06 18:11:06 -0300

    inicio da produção da api e reestilização de cores

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
 import express from 'express';
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`API subiu com sucesso na porta ${PORT}`);
+
+
+app.get('/', (req, res) => {
+    res.send('API FUNCIONANDOAAAAAAA')
+})
+
+app.get('/emails', (req, res) => {
+    res.json(
+        {
+            remetente: "Google",
+            assunto: "Sua conta foi invadida",
+            preview: "Altere sua senha imediatamente"
+        }
+    )
+})
+
+app.listen(7070, () => {
+    console.log(`API subiu com sucesso na porta 7070`);
 })
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import './index.scss'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { restaurarEmail, arquivarEmails, excluirEmail, marcarComoLido, marcarComoNaoLido, alternarImportante } from '../../utils/acoesEmails'
 
 import Header from '../../components/Header'
