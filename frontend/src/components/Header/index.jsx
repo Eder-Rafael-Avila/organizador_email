93dfe84f8bacb921ca9711a5b96629adf7b0cfb2
Eder-Rafael-Avila <ra52834094810@acaonsfatima.org.br>
2026-09-07 12:10:49 -0300

    Logo adicionada e reestilização de cores

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,10 +1,18 @@
 import './index.scss'
+import Logo from '../../assets/logo.png'
 
 export default function Header() {
 
     return (
         <header className='comp-header'>
-            <h1>Organizador de E-mails</h1>
+            <div className='imagem'>
+                <img src={Logo} />
+            </div>
+
+            <div className='slogan'>
+                <h1>Mailly</h1>
+                <p>Organize seus e-mails. Simplifique seu dia.</p>
+            </div>
         </header>
     );
 }
